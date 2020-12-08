c8223d183f1d66ce57b0d689656bab3a57031098
SakuraiGen <620sakurai@gmail.com>
2020-12-08 17:43:10 +0900

    StreamingAssrtsないのjson形式のファイルを読み取ってステージセレクト内の配列に入れる版

--- Assets/JsonArray.cs
+++ Assets/JsonArray.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class JsonArray : MonoBehaviour
 {
-    [SerializeField]
-    private string[] g_json_stage;
+    //ステージの名前を入れる配列
+    public string[] g_json_stage;
+
+    //フォルダーの数を数えるスクリプト
+    Folder_Script g_folder_Script;
+
+    //ステージ数
+    int g_stage_array_num;
     // Start is called before the first frame update
     void Start()
     {
+        //スクリプトを取得
+        g_folder_Script = GetComponent<Folder_Script>();
+
+        //フォルダーを数えるスクリプトの数値を入れる
+        g_stage_array_num = g_folder_Script.Filenum();
+
+        g_json_stage = new string[g_stage_array_num];
+
+        g_folder_Script.Filename();
         
     }
 
--- Assets/Scripts/Folder_Script.cs
+++ Assets/Scripts/Folder_Script.cs
@@ -13,13 +13,20 @@ public class Folder_Script : MonoBehaviour
 
     [SerializeField]
     int g_pathnum;
+
+    DirectoryInfo g_dir;
+
+    JsonArray g_json_array;
+
+    private int g_stagearray_pointer;
     void Start()
     {
+        g_json_array = GetComponent<JsonArray>();
         //指定した名前のフォルダ取得
-        DirectoryInfo dir = new DirectoryInfo(Application.streamingAssetsPath);
+       g_dir = new DirectoryInfo(Application.streamingAssetsPath);
 
         //指定したフォルダの中のフォルダーをすべて取得
-        g_dirarray = dir.GetDirectories("*");
+        g_dirarray = g_dir.GetDirectories("*");
         //フォルダの名前を表示
         foreach (DirectoryInfo d in g_dirarray)
         {
@@ -27,13 +34,32 @@ public class Folder_Script : MonoBehaviour
         }
 
         //指定したフォルダの名前指定
-        g_info = dir.GetFiles("*.json");
+        g_info = g_dir.GetFiles("*.json");
+
+    }
+    /// <summary>
+    /// ファイル数を数える
+    /// </summary>
+    /// <returns></returns>
+    public int Filenum() {
         //指定した名前のファイルの名前表示
         foreach (FileInfo f in g_info)
         {
             g_pathnum++;
             Debug.Log(g_pathnum);
             Debug.Log(f.Name);
+        }
+        return g_pathnum;
+    }
+    /// <summary>
+    /// 指定した形式のファイルを配列に入れる
+    /// </summary>
+    public void Filename() {
+
+        //指定した名前のファイルの名前表示
+        foreach (FileInfo f in g_info) {
+            g_json_array.g_json_stage[g_stagearray_pointer] = f.Name;
+             g_stagearray_pointer++;
         }
     }
     
