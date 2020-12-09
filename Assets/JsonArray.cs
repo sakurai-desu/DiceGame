ea0f310d5a70a4ce9efa1e6b3ca8fbab69bcd3ee
S.kikuchi <s.kikuchi456@gmail.com>
2020-12-09 13:36:51 +0900

    あらーーーー

--- Assets/JsonArray.cs
+++ Assets/JsonArray.cs
@@ -2,19 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JsonArray : MonoBehaviour
-{
+public class JsonArray : MonoBehaviour {
     //ステージの名前を入れる配列
     public string[] g_json_stage;
 
     //フォルダーの数を数えるスクリプト
     Folder_Script g_folder_Script;
 
+    //ステージの横の数
+    public int g_stage_side;
+
     //ステージ数
     public int g_stage_array_num;
+
+    //ステージの縦の数(最終的な縦の数を入れる)
+    public int g_stage_max_var;
+
+    //ステージの縦の数
+    public int g_stage_var;
+
+    //ステージのあまりの数
+    public int g_stage_remainder;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         //スクリプトを取得
         g_folder_Script = GetComponent<Folder_Script>();
 
@@ -24,12 +35,28 @@ public class JsonArray : MonoBehaviour
         g_json_stage = new string[g_stage_array_num];
 
         g_folder_Script.Filename();
-        
+
+        Stage_Calc();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
+    }
+    /// <summary>
+    /// ステージの縦との数を計算する
+    /// </summary>
+    public void Stage_Calc() {
+        //ファイル数とstageの横の数を割る
+        g_stage_var = g_stage_array_num / g_stage_side;
+
+        //ファイル数とstageの横の数を割った数の余りを求める
+        g_stage_remainder = g_stage_array_num % g_stage_side;
+        //割った数と余りを足して縦の数を決める
+        g_stage_max_var = g_stage_var + g_stage_remainder;
+
+        Debug.Log(g_stage_var);
+        Debug.Log(g_stage_remainder);
+        Debug.Log(g_stage_max_var);
     }
 }
