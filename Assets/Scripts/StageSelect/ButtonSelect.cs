ff6baa96ed1b04745a702ae7745fb149f8e4d111
S.kikuchi <s.kikuchi456@gmail.com>
2020-12-21 18:47:30 +0900

    競合したら相手で解決します。。。。

--- Assets/Scripts/StageSelect/ButtonSelect.cs
+++ Assets/Scripts/StageSelect/ButtonSelect.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonSelect : MonoBehaviour
-{
+public class ButtonSelect : MonoBehaviour {
     //ボタンの配列をつかさどる配列
     SpwernButton g_spwern_script;
     JsonArray g_json_script;
@@ -15,7 +14,7 @@ public class ButtonSelect : MonoBehaviour
     private int g_var_remainder;
     [SerializeField]
     int g_side_pointer;
-   
+
     [SerializeField]
     private int g_var_minicheck_pointer;
 
@@ -51,8 +50,7 @@ public class ButtonSelect : MonoBehaviour
     private float g_controller_time = 0.5f;
 
     private float g_limit_num = 0.49f;
-    void Start()
-    {
+    void Start() {
         //ボタンを生むのに必要な情報を取得
         g_array_Script = GameObject.Find("Stageinformation").GetComponent<JsonArray>();
 
@@ -60,20 +58,20 @@ public class ButtonSelect : MonoBehaviour
         g_spwern_script = GetComponent<SpwernButton>();
         //ボタンの幅を取得
         g_move_g_var = g_spwern_script.g_button_y_pos;
-       
+
         //横の数を取得
         g_side_stage_count = g_json_script.g_stage_side;
-      
-        g_page_var_count = g_spwern_script.g_var_size + 1　;
-       
+
+        g_page_var_count = g_spwern_script.g_var_size + 1;
+
         //生成するステージの数を取得
         g_max_stageelement = g_json_script.g_stage_array_num;
         //1ページのステージの数を計算
-        g_stage_page_count = g_side_stage_count * (g_page_var_count -1);
-      
+        g_stage_page_count = g_side_stage_count * (g_page_var_count - 1);
+
         if (g_max_stageelement % g_stage_page_count == 0) {
             Debug.Log("余り0");
-            g_var_remainder = g_page_var_count -1;
+            g_var_remainder = g_page_var_count - 1;
         } else {
             //最後のページのステージの個数
             g_stage_remainder = g_max_stageelement % g_stage_page_count;
@@ -88,13 +86,12 @@ public class ButtonSelect : MonoBehaviour
 
             }
         }
-      
+
     }
     bool g_oneflag;
     bool g_stick_flag;
 
-    void Update()
-    {
+    void Update() {
         if (g_oneflag == false) {
 
             ButtonBig();
@@ -102,7 +99,7 @@ public class ButtonSelect : MonoBehaviour
         }
         #region キーが押されたときの処理
         //Aキーが押されたとき
-        if (Input.GetKeyDown(KeyCode.A)||(Input.GetAxisRaw("Horizontal") <-g_controller_time && g_stick_flag==false)) {
+        if (Input.GetKeyDown(KeyCode.A) || (Input.GetAxisRaw("Horizontal") < -g_controller_time && g_stick_flag == false)) {
             ButtonOrigin();
             //２段目以降の一番左の時
             if (g_side_pointer == 0 && g_var_pointer != 0) {
@@ -111,7 +108,7 @@ public class ButtonSelect : MonoBehaviour
                 g_page_turnover -= 1;
 
                 if (g_page_turnover == 0) {
-                    this.transform.localPosition -= new Vector3(0, g_move_g_var * 20, 0);
+                    this.transform.localPosition -= new Vector3(0, g_move_g_var, 0);
                     g_page_turnover = g_page_var_count;
                     g_page_turnover -= 1;
 
@@ -120,11 +117,11 @@ public class ButtonSelect : MonoBehaviour
             //一番上の一番左の時
             else if (g_side_pointer == 0 && g_var_pointer == 0) {
                 g_var_pointer = g_spwern_script.g_varmax_num - 1;
-                g_side_pointer = g_spwern_script.g_side_num-g_spwern_script.g_remainder_num-1;
-                //g_page_turnover = g_var_remainder;
+                g_side_pointer = g_spwern_script.g_side_num - g_spwern_script.g_remainder_num - 1;
+                g_page_turnover = g_var_remainder;
                 //最後のページの縦の数を計算して取得
             } else {
-            g_side_pointer -= 1;
+                g_side_pointer -= 1;
             }
             ButtonBig();
             g_stick_flag = true;
@@ -133,12 +130,12 @@ public class ButtonSelect : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.D) || (Input.GetAxisRaw("Horizontal") > g_controller_time && g_stick_flag == false)) {
             ButtonOrigin();
             //一番下以外の一番右の時
-            if (g_side_pointer == g_spwern_script.g_side_num - 1 && g_var_pointer != g_spwern_script.g_varmax_num-1) {
+            if (g_side_pointer == g_spwern_script.g_side_num - 1 && g_var_pointer != g_spwern_script.g_varmax_num - 1) {
                 g_side_pointer = 0;
                 g_var_pointer += 1;
                 g_page_turnover += 1;
                 if (g_page_var_count == g_page_turnover) {
-                    this.transform.localPosition += new Vector3(0, g_move_g_var * 20, 0);
+                    this.transform.localPosition += new Vector3(0, g_move_g_var, 0);
                     g_page_turnover = 1;
                     ButtonBig();
                 }
@@ -162,15 +159,15 @@ public class ButtonSelect : MonoBehaviour
             g_stick_flag = true;
             //Debug.Log(g_spwern_script.g_json_stage_array[g_var_pointer, g_side_pointer]);
         }
-        if (Input.GetKeyDown(KeyCode.W)|| (Input.GetAxisRaw("Vertical") > g_controller_time && g_stick_flag == false)) {
+        if (Input.GetKeyDown(KeyCode.W) || (Input.GetAxisRaw("Vertical") > g_controller_time && g_stick_flag == false)) {
             ButtonOrigin();
             //ポインターがはみ出そうなとき
             if (g_var_pointer == 0) {
-                g_var_pointer = g_spwern_script.g_varmax_num-1;
+                g_var_pointer = g_spwern_script.g_varmax_num - 1;
                 g_var_pointer = VarNullChacker(0);
                 g_page_turnover = g_var_remainder;
             } else {
-            g_var_pointer -= 1;
+                g_var_pointer -= 1;
                 g_page_turnover -= 1;
             }
             ButtonBig();
@@ -178,29 +175,29 @@ public class ButtonSelect : MonoBehaviour
             //Debug.Log(g_spwern_script.g_json_stage_array[g_var_pointer, g_side_pointer]);
             //次のページに行く処理
             if (g_page_turnover == 0) {
-                this.transform.localPosition -= new Vector3(0, g_move_g_var * 20, 0);
+                this.transform.localPosition -= new Vector3(0, g_move_g_var , 0);
                 g_page_turnover = g_page_var_count;
                 g_page_turnover -= 1;
 
             }
         }
-        if (Input.GetKeyDown(KeyCode.S)||(Input.GetAxisRaw("Vertical") < -g_controller_time && g_stick_flag == false)) {
+        if (Input.GetKeyDown(KeyCode.S) || (Input.GetAxisRaw("Vertical") < -g_controller_time && g_stick_flag == false)) {
             ButtonOrigin();
             //ポインターがはみ出そうなとき
-            if (g_var_pointer == g_spwern_script.g_varmax_num-1) {
+            if (g_var_pointer == g_spwern_script.g_varmax_num - 1) {
                 g_var_pointer = 0;
                 g_page_turnover = 1;
             } else {
                 g_page_turnover += 1;
-            g_var_pointer += 1;
-            VarNullChacker(1);
+                g_var_pointer += 1;
+                VarNullChacker(1);
             }
             ButtonBig();
             g_stick_flag = true;
             //次のページに行く処理
-            if (g_page_var_count  == g_page_turnover ) {
-                this.transform.localPosition += new Vector3(0, g_move_g_var * 20, 0);
-                g_page_turnover =1;
+            if (g_page_var_count == g_page_turnover) {
+                this.transform.localPosition += new Vector3(0, g_move_g_var, 0);
+                g_page_turnover = 1;
                 ButtonBig();
             }
 
@@ -209,7 +206,7 @@ public class ButtonSelect : MonoBehaviour
         if (Input.GetAxisRaw("Vertical") > -g_limit_num && Input.GetAxisRaw("Vertical") < g_limit_num && Input.GetAxisRaw("Horizontal") < g_limit_num && Input.GetAxisRaw("Horizontal") > -g_limit_num && g_stick_flag) {
             g_stick_flag = false;
         }
-            if (Input.GetKeyDown(KeyCode.Space)||Input.GetButtonDown("B")) {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("B")) {
             g_stagename = g_spwern_script.g_json_button_array[g_var_pointer, g_side_pointer].GetComponent<GetStagename>();
             g_stagename.g_get_stagename = g_spwern_script.g_json_stage_array[g_var_pointer, g_side_pointer];
             g_stagename.OnClick();
@@ -245,7 +242,7 @@ public class ButtonSelect : MonoBehaviour
     private int SideNullChacker(int g_vectorenum) {
         while (true) {
             if (g_spwern_script.g_json_button_array[g_var_pointer, g_side_pointer] == null) {
-                g_side_pointer-=1;
+                g_side_pointer -= 1;
                 Debug.Log(g_side_pointer);
             } else {
                 return g_side_pointer;
@@ -256,8 +253,8 @@ public class ButtonSelect : MonoBehaviour
     /// 対処のものをでっかくするメソッド
     /// </summary>
     private void ButtonBig() {
-            g_buttonSize = g_spwern_script.g_json_button_array[g_var_pointer, g_side_pointer].GetComponent<ButtonSizeChange>();
-        
+        g_buttonSize = g_spwern_script.g_json_button_array[g_var_pointer, g_side_pointer].GetComponent<ButtonSizeChange>();
+
         g_buttonSize.BigButton();
     }
     /// <summary>
