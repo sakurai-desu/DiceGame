b484b94757fcae060330f54aa5d7d1b7eba53699
SakuraiGen <620sakurai@gmail.com>
2021-01-19 17:39:59 +0900

    リザルト政策

---
+++ Assets/TroubleScoreScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class TroubleScoreScript : MonoBehaviour
+{
+    /// <summary>
+    /// 手数が入ったスクリプト
+    /// </summary>
+    private ResultScript g_troubleScript = null;
+    /// <summary>
+    /// 手数を表示するためのテキスト
+    /// </summary>
+    private TextMeshProUGUI g_troubleText = null;
+
+    private void Start() {
+        g_troubleScript = GameObject.Find("Stageinformation").GetComponent<ResultScript>();
+        g_troubleText=this.GetComponent<TextMeshProUGUI>();
+        g_troubleText.text = g_troubleScript.Trouble().ToString();
+    }
+}
