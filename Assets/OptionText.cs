d66185f6434a039e9f9bdd762dcbc41fdfaf32c7
kimura <arumik0831@gmail.com>
2021-01-27 14:04:17 +0900

    フェードインアウト完成

--- Assets/OptionText.cs
+++ Assets/OptionText.cs
@@ -5,13 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class OptionText : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Fade_In_Out g_fade_Script;
+    private string g_scene_name = null;
+
     void Start()
     {
-        
+        g_fade_Script = GameObject.Find("Fade_Image").GetComponent<Fade_In_Out>();
     }
+
     public void SelectScene(string scenemane) {
-        SceneManager.LoadScene(scenemane);
+        g_fade_Script.Start_Fade_Out(Move_MainScene());
+        g_scene_name = scenemane;
     }
-  
+
+    private IEnumerator Move_MainScene() {
+        SceneManager.LoadScene(g_scene_name);
+        yield break;
+    }
 }
